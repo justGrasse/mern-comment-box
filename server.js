fffdef7477736b2923dfe0276a0e9e8a3f826fb9
Justin Grasse <just.grasse@gmail.com>
2017-07-15 16:37:13 -0700

    plugin Promise library to mongoose and add api routes for Comments

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ var router = express.Router();
 var port = process.env.API_PORT || 3001;
 
 //db config
-mongoose.connect(‘mongodb://mern-comment-box:mern-comment-box@ds161162.mlab.com:61162/mern-comment-box’);
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://mern-comment-box:mern-comment-box@ds161162.mlab.com:61162/mern-comment-box', {
+  useMongoClient: true,
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -28,6 +31,25 @@ app.use(function(req, res, next) {
 router.get('/', function(req, res) {
   res.json({ message: 'API Initialized!'});
 });
+
+router.route('/comments')
+ .get(function(req, res) {
+   Comment.find(function(err, comments) {
+     if (err) res.send(err);
+     res.json(comments)
+   });
+ })
+ .post(function(req, res) {
+   var comment = new Comment();
+   comment.author = req.body.author;
+   comment.text = req.body.text;
+   comment.save(function(err) {
+    if (err) res.send(err);
+    res.json({
+      message: 'Comment successfully added!',
+    });
+  });
+ });
 
 app.use('/api', router);
 
